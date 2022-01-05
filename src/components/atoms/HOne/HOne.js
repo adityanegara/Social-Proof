b8fcdeb7cc7134d790bdf96e3e53a7b1b0438cc7
adityanegara <aditnegarabisnis@gmail.com>
2022-01-05 16:31:53 +0700

    HOne used media query hooks

--- src/components/atoms/HOne/HOne.js
+++ src/components/atoms/HOne/HOne.js
@@ -1,8 +1,16 @@
-import styles from './HOne.module.scss';
+import useMediaQuery from '../../../hooks/useMediaQuery';
 
-const HOne = ({children}) =>{
+const HOne = ({children, fontSize, desktopAlign, mobileAlign, color}) =>{
+    const isDesktop = useMediaQuery('(min-width: 800px)');
+    const customStyles = {
+        hOne: isDesktop => ({
+          color: color,
+          fontSize: fontSize,
+          textAlign: isDesktop ? desktopAlign : mobileAlign,
+    })
+    }
     return(
-        <h1 style={{fontSize: '2.5em'}} className={styles['h-one']}>{children}</h1>
+        <h1 style={customStyles.hOne(isDesktop)} >{children}</h1>
     )
 }
 
--- src/components/molecules/Title/Title.js
+++ src/components/molecules/Title/Title.js
@@ -2,9 +2,10 @@ import styles from './Title.module.scss';
 import HOne from '../../atoms/HOne/HOne';
 
 const Title = ({title, description}) =>{
+
     return(
         <div>
-            <HOne>10,000+ of our users love our products.</HOne>
+            <HOne color='hsl(300, 43%, 22%)' fontSize={'2.5em'} desktopAlign={'left'} mobileAlign={'center'}>10,000+ of our users love our products.</HOne>
             <p>We only provide great products combined with excellent customer service. See what our satisfied customers are saying about our services.</p>
         </div>
     )
